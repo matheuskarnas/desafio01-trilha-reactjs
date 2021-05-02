c5a8ccac0a44800b8add350448d2f63edca89548
matheuskarnas <matheuskarnas1@gmail.com>
2021-05-02 06:35:20 -0300

    :tada: Challenge Finalized

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -15,15 +15,37 @@ export function TaskList() {
   const [newTaskTitle, setNewTaskTitle] = useState('');
 
   function handleCreateNewTask() {
-    // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    // Crie uma nova task com um id random, não permita criar caso o título seja vazio.   
+    if (newTaskTitle !== "") {
+
+      const newTask = {
+          id: Math.random(),
+          title: newTaskTitle,
+          isComplete: false,
+        }  
+      setTasks(oldState => [...oldState, newTask]) }
+      setNewTaskTitle('')
   }
 
   function handleToggleTaskCompletion(id: number) {
+
     // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    const checkBox = tasks.map(task => task.id === id ? {
+      ...task,
+      isComplete: !task.isComplete 
+    }: task );
+    
+    setTasks(checkBox)
+    
   }
 
   function handleRemoveTask(id: number) {
     // Remova uma task da listagem pelo ID
+    const filteredTasks = tasks.filter(task => task.id !== id);
+
+    setTasks(filteredTasks)
+    
+
   }
 
   return (
@@ -71,4 +93,4 @@ export function TaskList() {
       </main>
     </section>
   )
-}
+          }
